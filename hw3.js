e86993d85e6830f0633075eac6b4218f2cff06ce
your_name <marina.skorik@list.ru>
2023-07-11 17:03:38 +0300

    ДЗ 3 на проверку

---
+++ hw3.js
@@ -0,0 +1,104 @@
+// Задание 1
+let password = 'самый сложный пароль';
+let enteredPassword = prompt ('Введите пароль');
+if (password === enteredPassword) {
+  alert ("Пароль введен верно");
+} else {
+  alert ("Пароль введен не правильно");
+}
+
+
+// Задание 2 
+let c = 12;
+if (c>0 && c<10) {
+  console.log ('Верно');
+} else {
+  console.log ('Неверно')
+};
+c = 0;
+if (c>0 && c<10) {
+  console.log ('Верно');
+} else {
+  console.log ('Неверно')
+};
+c = 10;
+if (c>0 && c<10) {
+  console.log ('Верно');
+} else {
+  console.log ('Неверно')
+};
+c = -3;
+if (c>0 && c<10) {
+  console.log ('Верно');
+} else {
+  console.log ('Неверно')
+};
+c = 2;
+if (c>0 && c<10) {
+  console.log ('Верно');
+} else {
+  console.log ('Неверно')
+};
+
+
+// Задание 3
+let d = 120;
+let e = -5;
+if (d>100 || e>100) {
+  console.log ('Верно');
+} else {
+  console.log ('Неверно')
+};
+
+
+// Задание 4
+let a = '2';
+let b = '3';
+a = Number(a);
+b = Number(b);
+alert(a + b);
+
+
+// Задание 5 
+let monthNumber = prompt ("Введите номер месяца");
+switch (monthNumber) {
+  case "1":
+    alert("Зима");
+    break;
+  case "2":
+    alert("Зима");
+    break;
+  case "12":
+    alert("Зима");
+    break;  
+  case "3":
+    alert("Весна");
+    break;
+  case "4":
+    alert("Весна");
+    break;
+  case "5":
+    alert("Весна");
+    break;
+  case "6":
+    alert("Лето");
+    break;
+  case "7":
+    alert("Лето");
+    break;
+  case "8":
+    alert("Лето");
+    break;
+  case "9":
+    alert("Осень");
+    break;
+  case "10":
+    alert("Осень");
+    break;
+  case "11":
+    alert("Осень");
+    break;
+  default: 
+  alert("Такого месяца не существует");
+    break;
+}
--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-// Задание 1
-let a;
-a = 10;
-alert(a);
-a = 20;
-alert(a);
-// Задание 2
-let firstIPhone = "2007 год";
-alert (firstIPhone);
-// Задание 3 
-let creator = "Brendan Eich";
-alert (creator);
-// Задание 4 
-let c = 10;
-let b = 2;
-alert (c+b);
-alert (c-b);
-alert (c*b);
-alert (c/b);
-// Задание 5 
-let result = 2**5;
-alert (result);
-// Задание 6 
-a = 9;
-b = 2;
-alert (9%2);
-// Задание 7 
-let num =1;
-num +=5;
-num -=3;
-num *=7;
-num /=3;
-num++;
-num --;
-alert(num);
-// Задание 8 
-let age = prompt("Сколько вам лет?");
-alert(age);
-// Задание 9.0
-let user = {
-  name: "Julianna",
-  age: 35,
-  isAdmin: true
-}
-// Задание 9.1 
-user ['sity of residence'] = "Madrid";
-// Задание 9.2 
-user ['age'] = 36;
-// Задание 9.3 
-delete user ['sity of residence'];
-// Задание 9.4
-let info = prompt ('Какую информацию хотите узнать о пользователе?');
-console.log(user[info]);
-alert(user[info]);
-// Задание 10
-let userName = prompt ('Whats is your name?');
-alert (`Hello, ${userName}!`);
-//Дальше вопрос по 10-му заданию
-let name = prompt ('Whats is your name?');
-alert (`Hello, ${name}!`); //почему тут зачеркивает name?
+// // Задание 1
+// let a;
+// a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+// // Задание 2
+// let firstIPhone = "2007 год";
+// alert (firstIPhone);
+// // Задание 3 
+// let creator = "Brendan Eich";
+// alert (creator);
+// // Задание 4 
+// let c = 10;
+// let b = 2;
+// alert (c+b);
+// alert (c-b);
+// alert (c*b);
+// alert (c/b);
+// // Задание 5 
+// let result = 2**5;
+// alert (result);
+// // Задание 6 
+// a = 9;
+// b = 2;
+// alert (9%2);
+// // Задание 7 
+// let num =1;
+// num +=5;
+// num -=3;
+// num *=7;
+// num /=3;
+// num++;
+// num --;
+// alert(num);
+// // Задание 8 
+// let age = prompt("Сколько вам лет?");
+// alert(age);
+// // Задание 9.0
+// let user = {
+//   name: "Julianna",
+//   age: 35,
+//   isAdmin: true
+// }
+// // Задание 9.1 
+// user ['sity of residence'] = "Madrid";
+// // Задание 9.2 
+// user ['age'] = 36;
+// // Задание 9.3 
+// delete user ['sity of residence'];
+// // Задание 9.4
+// let info = prompt ('Какую информацию хотите узнать о пользователе?');
+// console.log(user[info]);
+// alert(user[info]);
+// // Задание 10
+// let userName = prompt ('Whats is your name?');
+// alert (`Hello, ${userName}!`);
+// //Дальше вопрос по 10-му заданию
+// let name = prompt ('Whats is your name?');
+// alert (`Hello, ${name}!`); //почему тут зачеркивает name?
